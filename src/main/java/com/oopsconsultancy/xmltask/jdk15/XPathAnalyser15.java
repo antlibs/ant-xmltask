b8bb9a09e371f4281e4704c839c490302b822b1c
twogee <g.grigelionis@gmail.com>
2020-01-06 18:50:18 +0100

    Provide XPathAnalyser with NamespaceContext

--- src/main/java/com/oopsconsultancy/xmltask/jdk15/XPathAnalyser15.java
+++ src/main/java/com/oopsconsultancy/xmltask/jdk15/XPathAnalyser15.java
@@ -2,15 +2,23 @@ package com.oopsconsultancy.xmltask.jdk15;
 
 import com.oopsconsultancy.xmltask.XPathAnalyser;
 import com.oopsconsultancy.xmltask.XPathAnalyserClient;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.ProcessingInstruction;
 
+import javax.xml.XMLConstants;
+import javax.xml.namespace.NamespaceContext;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * uses the JDK 1.5 XPath API
@@ -55,6 +63,13 @@ public class XPathAnalyser15 implements XPathAnalyser {
   public int analyse(Node node, String xpath) throws Exception {
     int count = 0;
     Object result = null;
+    Document doc = node.getOwnerDocument();
+    if (doc == null && node instanceof Document) {
+      doc = (Document) node;
+    }
+    if (doc != null) {
+      xPath.setNamespaceContext(new CachingNamespaceResolver(doc, false));
+    }
     try {
       result = xPath.evaluate(xpath, node, XPathConstants.NODESET);
     } catch (XPathExpressionException e) {
@@ -81,5 +96,115 @@ public class XPathAnalyser15 implements XPathAnalyser {
     }
 
     return count;
+  }
+
+  public class CachingNamespaceResolver implements NamespaceContext {
+    private static final String DEFAULT_NS = "DEFAULT";
+    private final Map<String, String> prefix2Uri = new HashMap<String, String>();
+    private final Map<String, String> uri2Prefix = new HashMap<String, String>();
+
+    /**
+     * This constructor checks the document and stores all namespaces it can
+     * find. If topLevelOnly is true, only namespaces in the root are used.
+     *
+     * @param document source document
+     * @param topLevelOnly restrict the search to enhance performance
+     */
+    public CachingNamespaceResolver(Document document, boolean topLevelOnly) {
+      examineNode(document.getFirstChild(), topLevelOnly);
+      System.out.println("The list of the cached namespaces:");
+      for (String key : prefix2Uri.keySet()) {
+        System.out.println("prefix " + key + ": uri " + prefix2Uri.get(key));
+      }
+    }
+
+    /**
+     * A single node is read, the namespace attributes are retrieved and stored.
+     *
+     * @param node to examine
+     * @param attributesOnly do not recurse if true
+     */
+    private void examineNode(Node node, boolean attributesOnly) {
+      if (node == null) {
+        return;
+      }
+
+      NamedNodeMap attributes = node.getAttributes();
+      if (attributes != null) {
+        for (int i = 0; i < attributes.getLength(); i++) {
+          Node attribute = attributes.item(i);
+          storeAttribute((Attr) attribute);
+        }
+      }
+
+      if (attributesOnly) {
+        return;
+      }
+
+      NodeList children = node.getChildNodes();
+      for (int i = 0; i < children.getLength(); i++) {
+        Node child = children.item(i);
+        if (child.getNodeType() == Node.ELEMENT_NODE) {
+          examineNode(child, false);
+        }
+      }
+    }
+
+    /**
+     * Look at an attribute and store it, if it is a namespace attribute.
+     *
+     * @param attribute to examine
+     */
+    private void storeAttribute(Attr attribute) {
+      // examine the attributes in namespace xmlns
+      if (attribute.getNamespaceURI() == null
+              || !attribute.getNamespaceURI().equals(XMLConstants.XMLNS_ATTRIBUTE_NS_URI)) {
+        return;
+      }
+
+      // Default namespace xmlns
+      if (attribute.getNodeName().equals(XMLConstants.XMLNS_ATTRIBUTE)) {
+        putInCache(DEFAULT_NS, attribute.getNodeValue());
+      } else {
+        // The defined prefixes
+        putInCache(attribute.getLocalName(), attribute.getNodeValue());
+      }
+    }
+
+    private void putInCache(String prefix, String uri) {
+      prefix2Uri.put(prefix, uri);
+      uri2Prefix.put(uri, prefix);
+    }
+
+    /**
+     * This method is called by XPath. It returns the default namespace, if the
+     * prefix is null or "".
+     *
+     * @param prefix to search for
+     * @return uri
+     */
+    public String getNamespaceURI(String prefix) {
+      if (prefix == null || prefix.equals(XMLConstants.DEFAULT_NS_PREFIX)) {
+        return prefix2Uri.get(DEFAULT_NS);
+      } else {
+        return prefix2Uri.get(prefix);
+      }
+    }
+
+    /**
+     * This method is not needed in this context, but can be implemented in a
+     * similar way.
+     */
+    public String getPrefix(String namespaceURI) {
+      return uri2Prefix.get(namespaceURI);
+    }
+
+    /**
+     * This method is required by the NamespaceContext interface but is not
+     * implemented.
+     */
+	public Iterator<String> getPrefixes(String namespaceURI) {
+        return null;
+    }
   }
 }
